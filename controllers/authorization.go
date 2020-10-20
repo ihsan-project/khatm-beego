f807f351934fc7d3027fcf87f7db102aa63fc92a
Mohammed Islam <mmislam101@gmail.com>
2020-10-20 13:00:39 -0700

    Respond differently

--- controllers/authorization.go
+++ controllers/authorization.go
@@ -15,6 +15,8 @@ type MyStruct struct {
 	payload string
 }
 
+var count int = 0
+
 func (this *AuthorizationController) Post() {
 	fmt.Println("authorizing: %d", time.Now())
 	// fmt.Println(this.Ctx.Input.RequestBody)
@@ -26,7 +28,14 @@ func (this *AuthorizationController) Post() {
 	jsoninfo := this.GetString("search_series")
 	fmt.Println(jsoninfo)
 
-	// this.Ctx.WriteString("done!")
+	// Respond 200
+	count++
 
+	// if count < 2 {
+	// 	fmt.Println("sending 400", count)
 	this.Abort("400")
+	// } else {
+	// 	fmt.Println("sending 200", count)
+	// 	this.Ctx.WriteString("done!")
+	// }
 }
